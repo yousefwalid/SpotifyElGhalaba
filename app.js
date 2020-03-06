b16da1800dca4be5bd512e888fcc7b65de112ea9
nasser <anasserabdrabo@gmail.com>
2020-03-06 16:30:47 +0200

    skeleton

---
+++ app.js
@@ -0,0 +1,32 @@
+//fundamental libs
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+//utils
+const appError = require('./utils/appError');
+const errorHandler = require('./controllers/errorController');
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+
+// 404, route not found
+app.use('*', (req, res, next) => {
+  const error = new appError("This route can't be found", 404);
+  next(error);
+});
+
+app.use(errorHandler);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: './config.env'
+});
+
+const app = require('./app');
+
+// remote database credentials
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  })
+});
+
+process.on("uncaughtException", err => {
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
