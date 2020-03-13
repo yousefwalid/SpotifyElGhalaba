1467b3fd139367d73ca28ef4c1894eb698eee966
MohamedAdel5 <mouhammedadel1999@gmail.com>
2020-03-13 11:33:26 +0200

    Added Security middlewares

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // Fundamental libs
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 const geoip = require('geoip-lite');
 const errorController = require('./controllers/errorController');
 
@@ -19,6 +25,24 @@ const meRouter = require('./routes/meRoutes');
 const AppError = require('./utils/appError');
 
 // 1) MIDDLEWARES
+
+// 1) set security http headers
+//Should be put at the top of the middleware stack
+//Adds security headers
+app.use(helmet());
+
+//2) Limit requests
+const limiter = rateLimit({
+  //limits 100 requests for each IP in one hour. If the IP exceeds this limit then it would have to wait for an hour to pass from the first request
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: {
+    status: 'fail',
+    message: 'Two many requests from this IP. please try again in an hour.'
+  }
+});
+app.use('/api', limiter);
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -26,13 +50,26 @@ if (process.env.NODE_ENV === 'development') {
 if (process.env.NODE_ENV === 'development') {
   app.enable('trust proxy');
 }
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-
+//4)Body parser and data sanitization
+//First: Reading data from the body of the request as json and converting it to javascript object into req.body
+app.use(express.json({ limit: '10kb' })); // The option limits the body data of the request to 10KB
+//Second: Data sanitization against NoSQL injection attacks.
+app.use(mongoSanitize());
+//Third: Data sanitization against XSS(cross-site scripting) attacks.
+app.use(xss());
+//Fourth: Prevent parameter pollution (prevents duplicate query string parameters)
+// app.use(
+//   hpp({
+//     whitelist: []
+//   })
+// );
+app.use(cookieParser());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.geoip = geoip.lookup(req.ip);
