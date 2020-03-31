3f10b1c55e46c1c9aed1b8f5803f7909e0ad16de
nasser <anasserabdrabo@gmail.com>
2020-03-31 02:41:21 +0200

    updating dropDB function

--- test/dropDB.js
+++ test/dropDB.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
-module.exports = async () => {
-    Object.keys(mongoose.connection.collections).forEach(async (collection) => {
-        await mongoose.connection.collections[collection].drop();
-    });
+module.exports = async (collectionName) => {
+    if (collectionName) {
+        await mongoose.connection.collections[collectionName].drop();
+        console.log(`✅ ${collectionName} dropped successfully`);
+    } else {
+        Object.keys(mongoose.connection.collections).forEach(async (collection) => {
+            await mongoose.connection.collections[collection].drop();
+        });
+
+        console.log(`✅ database dropped successfully`);
+    }
 };
