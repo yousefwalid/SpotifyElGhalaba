403774ba134941a363d3ecff6709bff81c1e695f
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2020-04-03 13:11:48 +0200

    Pipeline fix: Timeout the node process with code 0

--- server.js
+++ server.js
@@ -64,3 +64,9 @@ process.on('uncaughtException', err => {
 });
 
 process.on('warning', e => console.warn(e.stack));
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    process.exit(0);
+  });
+});
