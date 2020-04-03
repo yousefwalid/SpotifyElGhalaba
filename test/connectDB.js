c2c2ffe7b6c93affe04d68ca917d473a942a8dba
MohamedAdel5 <mouhammedadel1999@gmail.com>
2020-04-03 07:27:34 +0200

    Unittesting: Authentication module && Fixed dropDB function

--- test/connectDB.js
+++ test/connectDB.js
@@ -3,23 +3,28 @@ const dotenv = require('dotenv');
 
 // configuring .env
 dotenv.config({
-    path: './.test.env'
+  path: './.env.test'
 });
 
 // connecting to the db
 // remote database credentials
 const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
 );
 
 const connectDB = async () => {
+  try {
     await mongoose.connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
     });
-    console.log("✅ database connected");
+    console.log('✅ database connected');
+  } catch (err) {
+    console.log(`Error Connecting to DB ❌     ${err.toString()}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
