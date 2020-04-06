60bbc96fa1538bbac2ed7bd3d0984df5e15df2c9
Youssef Walid <yousefwalid98@hotmail.com>
2020-04-07 01:24:56 +0200

    updated functional doc of uploadAWSImage

--- utils/uploadAWSImage.js
+++ utils/uploadAWSImage.js
@@ -2,11 +2,12 @@ const sharp = require('sharp');
 const AwsS3Api = require('./awsS3Api');
 const AppError = require('./appError');
 /**
+ *  @module utilsModule
  *  Responsible for uploading images to AWS Bucket, please take care that each dimension has its corresponding qualityNames
  *  @param {Buffer} imageData The image data sent in the request,
  *  @param {String} modelName The string of the model this image is for, eg: album, artist
  *  @param {String} modelId The id of this instance of the model
- *  @param {Array<Array<Number>>} dimensions A 2D array containing the dimensions of the images, eg: [[50,50], [20,20]]
+ *  @param {Array<Array<Number>>} dimensions A 2D array containing the dimensions of the images, eg: [[50,50], [20,20]], each dimension is width*height
  *  @param {Array<String>} qualityNames An array containing the names of the qualities of each dimensions of photos, etc. High, Medium, Low.
  *  @returns {Array<imageObject>} An array containing the imageObjects of the images stored in AWS
  */
