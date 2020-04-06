a52803b893b8368905d0feb25489c0adfc3016bf
Youssef Walid <yousefwalid98@hotmail.com>
2020-04-06 22:26:24 +0200

    finalize server.js

--- server.js
+++ server.js
@@ -9,51 +9,34 @@ dotenv.config({
 
 const app = require('./app');
 
-(async () => {
-  await connectDB();
+connectDB().then();
 
-  const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-  const server = app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+// server.on('upgrade', authenticationController.protectWs);
+
+process.on('unhandledRejection', err => {
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
   });
+});
 
-  // server.on('upgrade', authenticationController.protectWs);
-
-  process.on('unhandledRejection', err => {
-    console.log(err);
-    server.close(() => {
-      process.exit(1);
-    });
+process.on('uncaughtException', err => {
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
   });
+});
 
-  process.on('uncaughtException', err => {
-    console.log(err);
-    server.close(() => {
-      process.exit(1);
-    });
+process.on('warning', e => console.warn(e.stack));
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    process.exit(0);
   });
-
-  process.on('warning', e => console.warn(e.stack));
-
-  process.on('SIGTERM', () => {
-    server.close(() => {
-      process.exit(0);
-    });
-  });
-})();
-
-// const DB = process.env.DATABASE;
-
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true
-//   })
-//   .then(() => console.log('DB connection successful! ✅'))
-//   .catch(err => {
-//     console.log(`Error Connecting to DB ❌     ${err.toString()}`);
-//     process.exit(1);
-//   });
+});
