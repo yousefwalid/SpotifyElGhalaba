89756cb341ca17ce0281c35dc4f309309c614d24
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2020-04-07 19:35:20 +0200

    Delete unneeded files

--- ___trash.js
+++
@@ -1,6 +0,0 @@
-require(`dotenv`).config({
-	path: "config.env"
-});
-require(`mongoose`).connect(`mongodb://localhost`, (err) => {
-	err && console.error(err);
-})
--- dockerize.sh
+++
@@ -1,7 +0,0 @@
-npm i
-docker network create sp8
-docker container run --name test_db --network sp8 mongo4-bionic
-# Make sure you have the write env vars, for a sure local deploy:
-mv ~/spotify/envvars/DOCKER_LOCAL_ENV ./config.env
-docker image build -t spotyify-el8alaba-back .
-docker run --name sp8-back --network sp8 spotyify-el8alaba-back:latest
