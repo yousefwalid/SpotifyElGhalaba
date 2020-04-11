908573ac79ceb12e59e1e925d83ae5deb38fb172
nasser <anasserabdrabo@gmail.com>
2020-04-11 22:21:14 +0200

    userController tests - making them independent

--- test/userController.spec.js
+++ test/userController.spec.js
@@ -1,60 +1,89 @@
 const assert = require('assert');
-const { dropDB } = require('./../utils/dropDB');
+const {
+    dropDB
+} = require('./../utils/dropDB');
 const createUser = require('./utils/createUser');
 const User = require('../models/userModel');
 const userController = require('../controllers/userController');
 
-describe('Testing user controller', function() {
-  const userToCreate = createUser();
-  userToCreate.email = userToCreate.email.toLowerCase();
-  let createdUser;
-  this.beforeAll('User', async () => {
-    await dropDB();
-  });
-
-  it('should add new user', async function() {
-    createdUser = await User.create(userToCreate);
-
-    Object.keys(userToCreate).forEach(key => {
-      if (key === 'password' || key === 'passwordConfirm') return;
-      assert.deepStrictEqual(userToCreate[key], createdUser[key]);
-    });
-  });
-
-  it('should get the user', async function() {
-    const user = await userController.getUserLogic(createdUser._id);
-
-    Object.keys(userToCreate).forEach(key => {
-      if (key === 'password' || key === 'passwordConfirm') return;
-      assert.deepStrictEqual(userToCreate[key], user[key]);
-    });
-  });
-
-  it('should get the user with specific fields only', async function() {
-    const user = await userController.getUserLogic(createdUser._id, 'name');
-
-    assert.ok(!Object.keys(user).includes('email'));
-  });
-
-  it('throw error if user is not found', async function() {
-    await assert.rejects(async () => {
-      await userController.getUserLogic('5e8281b93f83d84d5ab32e51');
-    });
-  });
-
-  it('should update the user info', async function() {
-    const user = await userController.updateUserLogic(createdUser._id, {
-      name: 'Nasser'
+describe('Testing user controller', function () {
+    const userToCreate = createUser();
+    userToCreate.email = userToCreate.email.toLowerCase();
+    let createdUser;
+    this.beforeAll('User', async () => {
+        await dropDB();
     });
 
-    assert.deepStrictEqual(user.name, 'Nasser');
-  });
+    it('should add new user', async function () {
+        const userBody = createUser();
+        userBody.email = userBody.email.toLowerCase();
+        const user = await User.create(userBody);
 
-  it('throw error if updating user not found', async function() {
-    await assert.rejects(async () => {
-      await userController.updateUserLogic('5e8281b93f83d84d5ab32e51', {
-        name: 'Nasser'
-      });
+        Object.keys(userBody).forEach(key => {
+            if (key === 'password' || key === 'passwordConfirm') return;
+            assert.deepStrictEqual(userBody[key], user[key]);
+        });
     });
-  });
+
+    it('should get the user', async function () {
+        const userBody = createUser();
+        userBody.email = userBody.email.toLowerCase();
+        const user = await User.create(userBody);
+
+        const returnedUser = await userController.getUserLogic(user._id);
+
+        Object.keys(userBody).forEach(key => {
+            if (key === 'password' || key === 'passwordConfirm') return;
+            assert.deepStrictEqual(userBody[key], returnedUser[key]);
+        });
+    });
+
+    it('should get the user with specific fields only', async function () {
+        const userBody = createUser();
+        userBody.email = userBody.email.toLowerCase();
+        const user = await User.create(userBody);
+        // console.log(user);
+
+        const returnedUser = await userController.getUserLogic(user._id, 'name');
+
+        assert.ok(!returnedUser.email);
+    });
+
+    it('throw error if user is not found', async function () {
+        const userBody = createUser();
+        userBody.email = userBody.email.toLowerCase();
+        const user = await User.create(userBody);
+
+        const userId = `${(user.id).substring(0, (user.id).length - 1)}${user.id[user.id.length] === 1? '2' : '1'}`;
+
+        await assert.rejects(async () => {
+            await userController.getUserLogic(userId);
+        });
+    });
+
+    it('should update the user info', async function () {
+        const userBody = createUser();
+        userBody.email = userBody.email.toLowerCase();
+        const user = await User.create(userBody);
+
+        const updatedUser = await userController.updateUserLogic(user._id, {
+            name: 'Nasser'
+        });
+
+        assert.deepStrictEqual(updatedUser.name, 'Nasser');
+    });
+
+    it('throw error if updating user not found', async function () {
+        const userBody = createUser();
+        userBody.email = userBody.email.toLowerCase();
+        const user = await User.create(userBody);
+
+        const userId = `${(user.id).substring(0, (user.id).length - 1)}${user.id[user.id.length] === 1? '2' : '1'}`;
+
+        await assert.rejects(async () => {
+            await userController.updateUserLogic(userId, {
+                name: 'Nasser'
+            });
+        });
+    });
 });
