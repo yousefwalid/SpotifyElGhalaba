d90e05d7b929abb9718872c2da471828b021fbe1
nasser <anasserabdrabo@gmail.com>
2020-04-11 22:05:14 +0200

    categoryController tests - making them independent

--- test/categoryController.spec.js
+++ test/categoryController.spec.js
@@ -4,37 +4,44 @@ const {
 } = require('../utils/dropDB');
 const categoryController = require('../controllers/categoryController');
 const generateCategory = require('./utils/generateCategory');
+const Category = require('../models/categoryModel');
 
 
 describe('Testing category controller', function () {
 
-    let category1Body;
-    let category2Body;
-    let category1;
-    let category2;
-
     this.beforeAll(async () => {
         await dropDB();
-        category1Body = await generateCategory();
-        category2Body = await generateCategory();
+
     });
 
     it('should create new categories', async function () {
+        const category1Body = await generateCategory();
+        const category2Body = await generateCategory();
+
         await assert.doesNotReject(async () => {
-            category1 = await categoryController.addCategoryLogic(category1Body);
-            category2 = await categoryController.addCategoryLogic(category2Body);
+            await categoryController.addCategoryLogic(category1Body);
+            await categoryController.addCategoryLogic(category2Body);
         });
     });
 
     it('get a category with id', async function () {
-        const category = await categoryController.getCategoryLogic(category1._id);
-        assert.ok(category);
-        assert.ok(category.name === category1Body.name);
+
+        const categoryBody = await generateCategory();
+        const category = await Category.create(categoryBody);
+
+        const returnedCategory = await categoryController.getCategoryLogic(category._id);
+
+        assert.ok(returnedCategory);
+        assert.ok(returnedCategory.name === category.name);
     });
 
     it('get a category with invalid id returns an error 404', async function () {
-        let categoryId = category1.id;
+        const categoryBody = await generateCategory();
+        const category = await Category.create(categoryBody);
+
+        let categoryId = category.id;
         categoryId = `${(categoryId).substring(0, categoryId.length - 1)}${categoryId[categoryId.length] === 1? '2' : '1'}`;
+
         try {
             assert.rejects(await categoryController.getCategoryLogic(categoryId));
         } catch (err) {
@@ -43,22 +50,26 @@ describe('Testing category controller', function () {
     });
 
     it('get all categories', async function () {
-        const names = [category1Body.name, category2Body.name];
+        const category1Body = await generateCategory();
+        const category2Body = await generateCategory();
+        const category1 = await Category.create(category1Body);
+        const category2 = await Category.create(category2Body);
+
         const categories = await categoryController.getAllCategoriesLogic({});
         const categoriesNames = categories.map(category => category.name);
 
-        names.forEach(name => {
-            assert.ok(categoriesNames.includes(name));
-        });
+        assert.ok(categoriesNames.includes(category1.name));
+        assert.ok(categoriesNames.includes(category2.name));
+
     });
 
     it('get categories playlists', async function () {
-        const categoryPlaylists = await categoryController.getCategoryPlaylistsLogic(category2.id, {});
+        const categoryBody = await generateCategory();
+        const category = await Category.create(categoryBody);
+
+        const categoryPlaylists = await categoryController.getCategoryPlaylistsLogic(category.id, {});
         categoryPlaylists.forEach(playlist => {
             assert.ok(playlist.type === 'playlist');
-        })
+        });
     });
-
-
-
 });
